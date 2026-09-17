91d1390dc5b80c8bd8dd238ca4ba9d90d9e1cb72
virtonen <virtonen@uni.minerva.edu>
2026-09-16 20:45:13 -0500

    changed line 74 to search for keyword in genre instead of title; removed favorite genre == genre condition in line 76

--- playlist_logic.py
+++ playlist_logic.py
@@ -71,9 +71,9 @@ def classify_song(song: Song, profile: Dict[str, object]) -> str:
     chill_keywords = ["lofi", "ambient", "sleep"]
 
     is_hype_keyword = any(k in genre for k in hype_keywords)
-    is_chill_keyword = any(k in title for k in chill_keywords)
+    is_chill_keyword = any(k in genre for k in chill_keywords)
 
-    if genre == favorite_genre or energy >= hype_min_energy or is_hype_keyword:
+    if energy >= hype_min_energy or is_hype_keyword:
         return "Hype"
     if energy <= chill_max_energy or is_chill_keyword:
         return "Chill"
